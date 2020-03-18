3cfa1054419d5a80c94b7f0592b14c14c7ae54cc
Josh Stroud <josh.e.stroud@gmail.com>
2020-03-18 15:42:43 -0700

    Retry 973 k closest points for faster solution

--- leetcode/973-k-closest-points.js
+++ leetcode/973-k-closest-points.js
@@ -3,7 +3,7 @@
  * @param {number} K
  * @return {number[][]}
  */
-var kClosest = function(points, K) {
+var kClosest1 = function(points, K) {
   let distanceTriples = points.map(pair => [...pair, calcDistance(...pair)]);
   let sortedDistanceTriples = distanceTriples.sort(tripleDistanceCompare);
 
@@ -36,6 +36,54 @@ const points1 = [
   [-2, 2]
 ];
 const K1 = 1;
+
+// Faster approach: use a quickselect similar to quicksort to sort each pair in place
+// This means we can sort pairs in O(n), and we can do in place
+// We choose a random pivot (middle of pairs, for example)
+// And have left and right pointer. We then go through each element in array, swapping if distance is less than or greater than pivot. Results in a sorted array in O(n) time.
+// Space complexity of O(1) if we do it in place
+
+// const kClosest = (pairs, K) => {
+//   const pivotIndex = Math.round((pairs.length - 1) / 2);
+//   const pivotPair = pairs[pivotIndex];
+
+//   const leftIndex = 0;
+//   const rightIndex = pairs.length - 1;
+
+//   while (leftIndex <= rightIndex) {
+//     const leftPivotCompare = comparePairs(pairs[leftIndex], pairs[pivotIndex]);
+
+//     while (leftIndex < rightIndex && leftPivotCompare === 1) {
+//       leftIndex++;
+//     }
+
+//     pairs[leftIndex] = pairs[rightIndex];
+
+//     const rightPivotCompare = comparePairs(
+//       pairs[rightIndex],
+//       pairs[pivotIndex]
+//     );
+
+//     while (leftIndex < rightIndex && rightPivotCompare === -1) {
+//       rightIndex--;
+//     }
+//   }
+
+//   return pairs
+// };
+
+const comparePairs = (a, b) => {
+  const distA = Math.sqrt(a[0] ** 2 + a[1] ** 2);
+  const distB = Math.sqrt(b[0] ** 2 + b[1] ** 2);
+
+  if (distA < distB) {
+    return -1;
+  } else if (distA > distB) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
 
 // console.log(kClosest(points1, K1));
 
