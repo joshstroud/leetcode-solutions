747ca4d80209fd721c82c8938ec272714420b0d5
Josh Stroud <josh.e.stroud@gmail.com>
2020-04-10 15:25:38 -0700

    Add #221 maximal square

--- leetcode/221-maximal-square.js
+++ leetcode/221-maximal-square.js
@@ -2,4 +2,67 @@
  * @param {character[][]} matrix
  * @return {number}
  */
-var maximalSquare = function (matrix) {};
+// Brute force: check every potential square on a grid. TC: m * n for each element, total of O(m^2 * n^2)
+// Notice: we can see if we build a square from every 1 corner, by checking three components: horizontal, vertical, diagonal, in each direction.
+// 1 1 1
+// 1 1 1
+// 1 1 1
+
+// simpler: check in each direction, so if we start at top right, then check horizontal and vertical, and create a seen as we build a chain. At each next element, check horizontal and vertical in direction
+// Another approach, for each 1, try to build a successively bigger square in each direction
+
+// After viewing solution, use dynamic programming approach
+// notice how we build a square: check top, diagonal left, and left squares. So for each bottom right square, we simply check the min of these three, and add one to them, and save to a matrix. Save max side length and square for area.
+// so matrix we build with previous example:
+// 1 1 1
+// 1 2 2
+// 1 2 3
+
+// 0 1 1
+// 0 1 1
+
+var maximalSquare = function (matrix) {
+  if (matrix.length === 1 && matrix[0].length === 1) {
+    return matrix[0][0];
+  }
+
+  const squareLengths = new Array(matrix.length);
+
+  for (let i = 0; i < squareLengths.length; i++) {
+    squareLengths[i] = [];
+  }
+
+  squareLengths[0][0] = matrix[0][0];
+  squareLengths[0][1] = matrix[0][1];
+  squareLengths[1][0] = matrix[1][0];
+
+  // squareLengths = 0 1 1
+  //                 0
+
+  let maxSideLength = 0;
+  for (let row = 1; row < matrix.length; row++) {
+    for (let col = 1; col < matrix[0].length; col++) {
+      if (matrix[row][col] === 1) {
+        const left = squareLengths[row][col - 1] || 0; //       1 1
+        const diagonal = squareLengths[row - 1][col - 1] || 0; //       0 1
+        const top = squareLengths[row - 1][col] || 0; //       0 1
+        const current = Math.min(left, top, diagonal) + 1; //       0 2
+
+        squareLengths[row][col] = current;
+        maxSideLength = Math.max(maxSideLength, current); //   1 2
+      }
+    }
+  }
+
+  // console.log(squareLengths);
+  return maxSideLength ** 2; // 4
+};
+
+const ex1 = [
+  [1, 0, 1, 0, 0],
+  [1, 0, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 0, 0, 1, 0],
+];
+
+console.log(maximalSquare(ex1));
--- leetcode/31-next-permutation.js
+++ leetcode/31-next-permutation.js
@@ -2,4 +2,33 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var nextPermutation = function (nums) {};
+// Approaches
+// Brute force: computer all possible permutations, and then rotate through list by one
+// TC: O(n!), SC: O(n!). Not good because slow and we want in-place constant space.
+// Notice we only need the *next* permutation, not all permutations.
+// Hypothetically we could compute the next permutation by simply comparing the numbers from the back of the list, and swapping them if they are less. Worst case is a TC of O(n) swap, where n is number of numbers. We could do in constant space.
+// Example: 1,2,3, compare 3 to 2, since > we swap and return
+//          1,3, 2, compare 3 to 2, skip, and now compare these two with first, and swap, so 2, 1, 3, and then start over from back
+//          2, 1, 3, compare 3 to 1 and swap so 2, 3, 1
+//          2, 3, 1, compare 3 1 don't swap, compare 2 with 3, get 3, and then 
+
+// Notice: calculating permutation of last pair and then increasing by 1 and running an algorithm on this
+// 1,2,3,4
+// 1,2,4,3 (math.max of last two, math.min of last two)
+// 1,3,2,4 (math.min of last two swap with first, then math.min of last two, then math.max of last two)
+// 1,3,4,2 (math.max of last two, then math.min of last two)
+// 1,4,2,3 (math.max of last two, then math.min of last two, then math.max of last two)
+// 1,4,3,2 (math.max of last two, then math.min of last two)
+// 2,1,3,4 (math.min of last three, then math.min of last three, then, math.min of )
+
+// Can we use info that we know will be in ascending order? Can we use a Math min/max/sort? Sort is too slow (nlogn), but Math.min/max is constant time so sounds promising.
+// We know for the three-el we want to first swap min 2, and then swap next biggest num
+
+// base case: one element, don't swap
+
+
+
+
+
+
+ var nextPermutation = function (nums) {};
